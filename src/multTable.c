a176825182be7c3fd6fe368902eb8b27880f7cb1
Emansyed33 <144342740+Emansyed33@users.noreply.github.com>
2023-10-22 13:45:18 +0500

    for loop used to fill this empty table

--- src/multTable.c
+++ src/multTable.c
@@ -1,6 +1,6 @@
 /**
- * Author: 
- * Date: 
+ * Author: Eman SYED
+ * Date:   22/10/2023
  *
  * This program generates a multiplication table
  * of the size given as a command line argument.
@@ -19,6 +19,12 @@ int main(int argc, char **argv) {
   if(n <= 0) {
     printf("empty table!\n");
     exit(0);
+  } 
+  for(int i = 1; i <= n; i++){
+    for(int j = 1; j <= n; j++) {
+      printf("%4d", i * j);
+    }
+    printf("\n");
   }
 
   //TODO: place your code here
